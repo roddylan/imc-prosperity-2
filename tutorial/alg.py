33032ceb2866eedd2d5da62dc7d22f4f513bffd5
Roderick Lan <rodericklan03@gmail.com>
2024-03-01 02:59:49 -0700

    datamodel.py

---
+++ tutorial/alg.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+class TradingState(object):
+    def __init__(self):
+        pass
+
+class Trader:
+    def __init__(self):
+        pass
+
+
+
+    def run(self, state: TradingState):
+        # all trading logic
+        pass
