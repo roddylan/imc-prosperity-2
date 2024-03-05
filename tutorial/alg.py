55a0ff91616692b202f2ae8bdfbbcfbb2bc54778
Roderick Lan <rodericklan03@gmail.com>
2024-03-04 21:03:43 -0700

    empty

--- tutorial/alg.py
+++ tutorial/alg.py
@@ -2,11 +2,16 @@ from datamodel import OrderDepth, UserId, TradingState, Order
 import numpy as np
 
 class Trader:
-    def __init__(self):
-        pass
+    # def __init__(self):
+    #     pass
 
 
 
     def run(self, state: TradingState):
         # all trading logic
-        pass
+        
+        traderData = "" # TODO: finish
+        
+        
+        print(state)
+        # pass
---
+++ tutorial/utils.py
@@ -0,0 +1,10 @@
+from datamodel import OrderDepth, UserId, TradingState, Order
+import pandas as pd
+import numpy as np
+
+
+
+
+class TradingStateOverview:
+    def __init__(self, state: TradingState):
+        pass
